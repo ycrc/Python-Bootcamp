ac25c988dc1cb6cebbc7e5bcd0b7075f71150e98
Robert Bjornson <robert.bjornson@yale.edu>
2016-04-11 14:53:13 -0400

    various changes

--- walk.py
+++ walk.py
@@ -1,8 +1,17 @@
-
-import os, sys
-
+import os, sys, subprocess
 start=sys.argv[1]
 for d, subdirs, files in os.walk(start):
-    print d, subdirs, files
+    for f in files:
+        if f.endswith('.fastq.qp'):
+            fn=d+'/'+f
+            nfn=fn.replace('.fastq.qp', '.fastq')
+            cmd='quip -d '+fn+' > '+nfn
+            ret=subprocess.call(cmd, shell=True)
+            if ret==0:
+                if os.path.exists(nfn):
+                    os.remove(fpn)
+            else:
+                print "Failed on ", fn
+                sys.exit(1)
 
             
