4356ddce983cf29bce79be3484429cc389212a74
Robert Bjornson <robert.bjornson@yale.edu>
2016-04-12 10:14:22 -0400

    changed walk example to do compression

--- walk.py
+++ walk.py
@@ -2,10 +2,10 @@ import os, sys, subprocess
 start=sys.argv[1]
 for d, subdirs, files in os.walk(start):
     for f in files:
-        if f.endswith('.fastq.qp'):
+        if f.endswith('.fastq'):
             fn=d+'/'+f
-            nfn=fn.replace('.fastq.qp', '.fastq')
-            cmd='quip -d '+fn+' > '+nfn
+            nfn=fn.replace('.fastq', '.fastq.qp')
+            cmd='quip -c '+fn+' > '+nfn
             ret=subprocess.call(cmd, shell=True)
             if ret==0:
                 if os.path.exists(nfn):
