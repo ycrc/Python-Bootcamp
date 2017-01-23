0964e13bf241de672a369bdc972be3d8fdcd8613
Robert Bjornson <robert.bjornson@yale.edu>
2017-01-23 17:54:56 -0500

    lots of changes

--- Ex2.py
+++ Ex2.py
@@ -1,17 +1,17 @@
 import os, sys, subprocess
-start=sys.argv[1]
+# sys.argv=['dummy', 'Ex2dir'] # for Jupyter we'll cheat
+start=sys.argv[1] 
 for d, subdirs, files in os.walk(start):
     for f in files:
         if f.endswith('.fastq'):
             fn=d+'/'+f
-            nfn=fn.replace('.fastq', '.fastq.qp')
-            cmd='quip -c '+fn+' > '+nfn
+            nfn=fn.replace('.fastq', '.fastq.gz')
+            cmd='gzip -c '+fn+' > '+nfn
+            print ("running", cmd)
             ret=subprocess.call(cmd, shell=True)
             if ret==0:
                 if os.path.exists(nfn):
                     os.remove(fn)
             else:
-                print "Failed on ", fn
+                print ("Failed on ", fn)
                 sys.exit(1)
-
-            
---
+++ Ex3.py
@@ -0,0 +1,30 @@
+'''
+Example 3
+This script reads a file containing genes and contructs a lookup table
+It then reads a file containing mapped reads and finds the genes
+overlapping with each read.
+'''
+
+import sys
+from intervaltree import IntervalTree
+
+print "initializing table"
+table={}
+# sys.argv=['dummy', 'genes.txt', 'mappedreads.txt'] # for Jupyter
+for line in open(sys.argv[1]):
+    genename, chrm, strand, start, end = line.split()
+    if not chrm in table:
+        table[chrm]=IntervalTree()
+    table[chrm][int(start):int(end)]=genename
+print "done"
+
+print "reading sequences"
+
+for line in open(sys.argv[2]):
+    name, chrm, pos, seq = line.strip().split()
+    genes=table[chrm][int(pos):int(pos)+len(seq)]
+    if genes:
+        print name, chrm, pos, seq
+        for gene in genes:
+            print '\t',gene.data
+
