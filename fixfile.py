d934955c1e404e282d996756e001892c36720423
robert bjornson <rbjornson@gmail.com>
2017-01-23 11:19:00 -0500

    interim

--- fixfile.py
+++ fixfile.py
@@ -1,7 +1,7 @@
 import sys
 fp=open(sys.argv[1])
-print fp.readline().strip()
+print (fp.readline().strip())
 for l in fp:
    flds=l.strip().split(',')
    flds[4]=flds[4][:-3]
-   print ','.join(flds)
+   print (','.join(flds))
